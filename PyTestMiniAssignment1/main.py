df4fd8c873dd7dc7760418cbef357746e732774f
Verma <priyankverma@deloitte.com>
2022-05-17 23:52:26 +0530

    String methods added

---
+++ PyTestMiniAssignment1/main.py
@@ -0,0 +1,7 @@
+import StringMethods
+
+
+if __name__ == '__main__':
+    stringClassobj = StringMethods.StringClass
+    stringClassobj.LengthOfString("12345678")
+    stringClassobj.ListOfChar("12345678")
