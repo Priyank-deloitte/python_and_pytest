ce9e64fcbfdf29724eefc9f110cbd96ccc7f6e78
Verma <priyankverma@deloitte.com>
2022-05-18 00:52:16 +0530

    PairsPossible methods added

--- PyTestMiniAssignment1/main.py
+++ PyTestMiniAssignment1/main.py
@@ -1,7 +1,11 @@
 import StringMethods
+import PairsMethods
 
 
 if __name__ == '__main__':
     stringClassobj = StringMethods.StringClass
     stringClassobj.LengthOfString("12345678")
     stringClassobj.ListOfChar("12345678")
+
+    pairObj = PairsMethods.PairsPossible
+    pairObj.pairs("13246587")
